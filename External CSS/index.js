49684bffee6b39785c04e504b5d440f6d467291d
Dibyendu Biswas <67157274+dibyendubiswas1998@users.noreply.github.com>
2021-03-01 12:05:17 +0530

    Add files via upload

---
+++ External CSS/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// External CSS:---
+
+/* Import 'index.css' file like this */
+import './index.css';
+
+
+ReactDOM.render(
+  <div className="ele">
+      <h1>Hello All</h1>
+      <h4>External CSS</h4>
+      <p>Text-1</p>
+      <p>Text-2</p>
+      <p>Text-3</p>
+      
+      
+  </div>,
+  document.getElementById('root')
+);
+
+
