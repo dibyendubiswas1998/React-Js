92072e7d407ad91d7b17f13d4132844fc5f45323
Dibyendu Biswas <67157274+dibyendubiswas1998@users.noreply.github.com>
2021-03-01 17:53:53 +0530

    Add files via upload

---
+++ Props/Example-04/Sdata.jsx
@@ -0,0 +1,32 @@
+const Data = [
+    {
+        id: 1,
+        imgsrc : "https://picsum.photos/200/300?grayscale",
+        altname : "img1",
+        title : "Image 1",
+        description : "This is image-one",
+    },
+    {
+        id: 2,
+        imgsrc : "https://picsum.photos/id/870/200/300?grayscale&blur=2",
+        altname : "img2",
+        title : "Image 2",
+        description : "This is image-two",
+    },
+    {
+        id: 3,
+        imgsrc : "https://picsum.photos/seed/picsum/200/300",
+        altname : "img3",
+        title : "Image 3",
+        description : "This is image-three",
+    },
+    {
+        id: 4,
+        imgsrc : "https://picsum.photos/200/300?grayscale",
+        altname : "img4",
+        title : "Image 4",
+        description : "This is image-four",
+    },
+]
+
+export default Data;
---
+++ Props/Example-04/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Cards';
+import './index.css';
+import Data from './Sdata';
+
+// props:----
+
+
+// using map method call all data from Sdata.
+function CallAll(val){
+  return(
+    <Card
+         imgsrc = {val.imgsrc}
+         altname = {val.altname}
+         title = {val.title}
+         description = {val.description}
+    />
+  );
+};
+
+ReactDOM.render(
+  <>
+    {/* using map method display all Element or Data */}
+    {Data.map(CallAll)}
+
+  </>,
+  document.getElementById('root')
+);
