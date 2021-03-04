1ff0fdacb27f53f351ae17018dcb5f7c6f4158ca
Dibyendu Biswas <67157274+dibyendubiswas1998@users.noreply.github.com>
2021-03-04 11:57:51 +0530

    Add files via upload

---
+++ React Router/Menu or Nav Bar/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Menu from './Menu';
+//  Import Route, Switch 'from react-router-dom'
+import {Route, Switch} from 'react-router-dom';
+
+// import all pages (Home, About, Services, etc.)
+import Home from './Home';
+import Blog from './Blog';
+import About from './About';
+import Services from './Services';
+// For Error page
+import Error from './Error';
+
+const App = () =>{
+    return(
+        <>
+            <Menu/>
+            <Switch>
+                {/* 
+                    "path" mean --> where u want to visit &&
+                    "component" mean --> exactly this particular page/component.
+                    "exact" mean --> exactly this paage, otherwise show error.
+                */}
+                <Route exact path='/' component={Home} /> {/* By Default this is home page or main or opening page, when path='/' */}
+                <Route exact path='/about' component={About} /> 
+                <Route exact path='/blog' component={Blog} /> 
+                <Route exact path='/services' component={Services} /> 
+
+                {/* 
+                    When you have no page then show that this is error page, means
+                    you have Home, Blog, About, Services pages. But user try to search another page
+                    which is actually not exits in your system. Then it's showing error that this page
+                    is not found.
+                */}
+                <Route component={Error} />
+            </Switch>
+        </>
+    )
+};
+export default App;
---
+++ React Router/Menu or Nav Bar/Error.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Error = ()=>{
+    return(
+        <h1>This is Error Page</h1>
+    )
+};
+export default Error;
